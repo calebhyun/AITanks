2d6e292670271bc7cae5d2eae7d3ecb977f65ba9
calebhyun <calebhyun@live.com>
2020-03-24 09:56:33 -0700

    Do nothing program

---
+++ AICaleb1.py
@@ -0,0 +1,13 @@
+import inspect
+if "rungame" not in inspect.getmodule(inspect.stack()[0])._filesbymodname["__main__"]:
+    import rungame
+
+class AI:
+    def __init__(self):
+        print(__name__ + " AI Loaded")
+        self.image = "tankblue4.png"
+
+    def turn(self):
+        print("Do Nothing")
+
+            
