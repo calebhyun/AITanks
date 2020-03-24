90c06fd8fadb2c298b2adc528dfd0d4f794abcb8
calebhyun <calebhyun@live.com>
2020-03-24 10:32:00 -0700

    print("check in")

--- AICaleb1.py
+++ AICaleb1.py
@@ -6,8 +6,9 @@ class AI:
     def __init__(self):
         print(__name__ + " AI Loaded")
         self.image = "tankblue4.png"
-
+        
     def turn(self):
         print("Do Nothing")
+        print("Check in!")
 
             
